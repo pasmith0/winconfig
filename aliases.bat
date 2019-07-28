46fa0ae67729b526a4ef2f932908b7c978a9c5c1
Paul Smith <paul@psmith.me>
2019-07-28 09:57:50 -0600

    Add aliases (unused)

---
+++ aliases.bat
@@ -0,0 +1,17 @@
+@echo off
+
+doskey alias=if ".$*." == ".." (doskey /macros) else (doskey $*)
+rem should have unalias command
+doskey unalias=doskey $*=
+
+doskey vi=gvim $*
+rem doskey edit="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
+doskey l=dir/ogn $*
+
+rem Turn off prompting for date/time
+doskey time=time/t 
+doskey date=date/t
+
+rem Default editor - should be in path
+doskey edit=uedit64 $*
+
